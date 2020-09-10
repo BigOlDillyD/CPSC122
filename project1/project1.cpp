043dab7a9dc3a869346eb93188bd76bd8958a9c9
Nathan Mautz <nmautz@ada.gonzaga.edu>
2020-09-10 16:05:20 -0700

    Removed error checks, fixed spacing, removed unnessisary coments

--- project1/project1.cpp
+++ project1/project1.cpp
@@ -20,162 +20,151 @@ To Execute: ./project1
 #include <fstream>
 
 
-
-
-
+/*
+Pre: Integer is passed as argument
+Post: Program will return true if num is prime, and false if num is not prime.
+*/
 bool IsPrime(int num){
 
-	
 
-	if(num <= 1) return false;
-	
-	for(int i = 2; i <= (num/2); i++){
-	
-		if(num % i == 0){	
-			return false;
-			
-		}
 
-	
-	}
-	
-	return true;
+
+   if(num <= 1) return false;
+   
+   for(int i = 2; i <= (num/2); i++){
+   
+      if(num % i == 0){ 
+         return false;
+         
+      }
+
+   
+   }
+   
+   return true;
 
 }
 
-
+/*
+Pre: count must be >=1
+Post: Will return pointer to an array of prime numbers 
+*/
 
 
 int* GeneratePrimeNumbers(int count){ 
-	
-	//Creates int array of size count on the heap
-	int *primeNums = new int[count];
-	
-	//Seeds random number generator
-	srand(time(0));
-	
+   
+   int *primeNums = new int[count];
+   
+   srand(time(0));
+   
 
-	int num = 0;
-	
-	
-	//Generates prime numbers
-	for(int i = 0; i < count; i++){
-		
-		bool found = false;
+   int num = 0;
+   
+   
+   //Generates prime numbers
+   for(int i = 0; i < count; i++){
+      
+      bool found = false;
 
-		//Generates a random number, then checks if said number is prime.
-		//If that number is prime, it is added to the primeNums array
-		//the boolean "found" is then set to true so the loop can end.
-		while(!found){
+      while(!found){
 
-			num = rand()%100;
-			if(IsPrime(num)){
-				found = true;
-				primeNums[i] = num;
-			}
-		}
+         num = rand()%100;
+         if(IsPrime(num)){
+            found = true;
+            primeNums[i] = num;
+         }
+      }
 
-	}
-	
-	return primeNums;
+   }
+   
+   return primeNums;
 
 }
 
 
 
 void PrintPrimeNums(int count, int columns, std::ofstream& outputFile){
-	
-	
-	
-	int index = 0;
-	int columnIndex = 0;
+   
+   
+   
+   int index = 0;
+   int columnIndex = 0;
 
-	int *primeNums = GeneratePrimeNumbers(count);
-	
-	while(index < count){
-	
-		if(columnIndex < columns){
-		
-			outputFile << primeNums[index] << "   ";
-			index++;
-			columnIndex++;
-			
-		
-		}else{
-		
-			columnIndex = 0;
-			outputFile << std::endl;
-		
-		}
-	
+   int *primeNums = GeneratePrimeNumbers(count);
+   
+   while(index < count){
+   
+      if(columnIndex < columns){
+         
+         outputFile << primeNums[index] << "   ";
+         index++;
+         columnIndex++;
+         
+      
+      }else{
+      
+         columnIndex = 0;
+         outputFile << std::endl;
+      
+      }
+   
 
-	}
-	
-	
-	//Memory allocated on heap is destroyed
-	delete[] primeNums;
+   }
+   
+   delete[] primeNums;
 
 
 }
-	
+   
 
-
-
+/*
+Pre: None
+Post: Will return pointer to array of integers that hold the users input
+*/
+   
 int* GetInput()
 {
-	int *input = new int[2];
+   int *input = new int[2];
 
-	std::cout << "How many prime numbers would you like to generate" << std::endl;
-	
-	while(std::cin >> input[0], std::cin.fail())
-	{
-		std::cout << "Input Error" << std::endl;
+   std::cout << "How many prime numbers would you like to generate" << std::endl;
+   
+   std::cin >> input[0];   
+   
+   
+   std::cout << "How many columns would you like?" << std::endl;
+   
+   std::cin >> input[1];
 
-		std::cin.clear();
-		std::cin.ignore(10000, '\n');
-	}	
-	
-	
-	std::cout << "How many columns would you like?" << std::endl;
-	
-	while(std::cin >> input[1], std::cin.fail())
-	{
-		std::cout << "Input Error" << std::endl;
-
-		std::cin.clear();
-		std::cin.ignore(10000, '\n');
-
-	}	
-
-	return input;
+   
+   return input;
 
 }
 
 
 int main(int argc, char* argv[]){
 
-	
-	
-	
-	if(argc != 2){
-		std::cout << "Incorrect count of arguments given: " << argc << std::endl;
-		return 0;
-	
-	
-	}
-	
-	int *input = GetInput();	
-		
-	std::ofstream outputFile;
-	outputFile.open(argv[1]);
-	  	
+   
+   
+   
+   if(argc != 2){
+      std::cout << "Incorrect count of arguments given: " << argc << std::endl;
+      return 0;
+   
+   
+   }
+   
+   int *input = GetInput();   
+      
+   std::ofstream outputFile;
+   outputFile.open(argv[1]);
+      
 
-  	PrintPrimeNums(input[0], input[1], outputFile);
-  	
-  	delete[] input;
-	
-	outputFile.close();
+   PrintPrimeNums(input[0], input[1], outputFile);
+   
+   delete[] input;
+   
+   outputFile.close();
 
 
-	return 0; 
+   return 0; 
 }
