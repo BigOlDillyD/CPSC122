87aee6573829d22ea8b1e3bce66f54667e31d5b5
Nathan Mautz <nmautz@ada.gonzaga.edu>
2020-10-27 20:02:57 -0700

    yeah

---
+++ project6/project6Tst.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "list.h"
+
+using namespace std;
+
+
+
+
+int main()
+{
+ //Use of a static list 
+ List lst;
+ for (int i = 0; i < 5; i++)
+   lst.PutItemH(i);
+ lst.Print();
+
+ //Use of a dynamic list
+ List* lst1 = new List;
+ for (int i = 0; i < 5; i++)
+  lst1->PutItemH(10*i);
+ lst1->Print();
+
+ delete lst1; //necessary to invoke destructor on dynamic list
+ return 0;
+}
